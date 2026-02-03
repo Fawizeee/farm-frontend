2c8aafeffa34a61767bbeea73ee3df79bbe49eb6
unknown <Bellofawaz5@gmail.com>
2026-02-03 22:01:25 +0100

    err fixed

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Create an axios instance with default config
 // Set REACT_APP_API_URL in your .env file
-const enviroment = "lh";
-const baseURL = enviroment === "" ? "http://localhost:8000" : process.env.REACT_APP_API_URL;
+
+const baseURL = process.env.REACT_APP_API_URL;
 console.log('API Client initialized with baseURL:', baseURL);
 
 const apiClient = axios.create({
